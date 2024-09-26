534e91ff4b4b66b4a2c1db1e9c77a2c5a7d920d3
Adithi <113155833+Adithi13@users.noreply.github.com>
2024-09-26 12:43:53 +0530

    Find all the non-repeating elements in an array

---
+++ Array/Find all the non-repeating elements in an array.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    vector<int> arr;  // Declare a vector to store the elements
+
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < n; i++) {
+        int element;
+        cin >> element;  // Input each element
+        arr.push_back(element);  // Push the element into the vector
+    }
+
+    unordered_map<int, int> count;
+    
+    // Count the frequency of each element
+    for (auto i : arr) {
+        count[i]++;
+    }
+
+    cout << "The repeated elements of the array are: ";
+
+    // Print elements that appear more than once
+    for (auto i : count) {
+        if (i.second < 2) {  // Access the frequency using i.second
+            cout << i.first << " ";  // Print the element using i.first
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
