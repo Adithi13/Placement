2d884a84a4906ec2de7551dc1d444b0ea3cd961e
Adithi <113155833+Adithi13@users.noreply.github.com>
2024-10-18 22:44:08 +0530

    Check if a number is Armstrong Number or not

---
+++ Numbers/Check if a number is Armstrong Number or not.py
@@ -0,0 +1,13 @@
+num = int(input("ente a number: "))
+order = len(str(num))
+sum = 0
+temp = num
+while temp>0:
+    digit = num % 10
+    sum += digit ** order
+    temp //= 10
+
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
